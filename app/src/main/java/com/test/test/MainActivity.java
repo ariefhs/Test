db23024ce5fa10fd4f2db0d93342beabc4c419c0
Arief Hidayat Sutomo <arief.hidayat@bigio.id>
2019-12-11 10:14:54 +0700

    initial commit

---
+++ app/src/main/java/com/test/test/MainActivity.java
@@ -0,0 +1,41 @@
+package com.test.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText value_a, value_b, value_result;
+    Button btnCalculate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        value_a = findViewById(R.id.value_a);
+        value_b = findViewById(R.id.value_b);
+        value_result = findViewById(R.id.value_result);
+
+        btnCalculate = findViewById(R.id.calculate);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint({"SetTextI18n"})
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(value_a.getText()) || TextUtils.isEmpty(value_b.getText())) {
+                    Toast.makeText(getApplicationContext(), "value A / value B cannot empty.", Toast.LENGTH_LONG).show();
+                } else {
+                    int result = Integer.parseInt(value_a.getText().toString()) + Integer.parseInt(value_b.getText().toString());
+                    value_result.setText(""+result);
+                }
+            }
+        });
+    }
+}
